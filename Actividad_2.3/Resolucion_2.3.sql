ac33d9e03c3879e08215c1801a34ff921571c4f3
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2023-05-18 16:58:29 -0300

    3 y 4 resueltos

--- Actividad_2.3/Resolucion_2.3.sql
+++ Actividad_2.3/Resolucion_2.3.sql
@@ -7,7 +7,8 @@
 --	select   idtipoinfraccion, avg(monto) from multas
 --	group by IdTipoInfraccion
 --	order by idTipoInfraccion asc
-
+select * from mediosPago
+select * from Pagos
 
 	
 	
@@ -21,21 +22,83 @@
 
 
 --4 Listado con la cantidad de pagos que se realizaron.
+		select * from multas 
+		where pagada = 0
+
 --5 Listado con la cantidad de multas realizadas en la provincia de Buenos Aires.
 --NOTA: Utilizar el nombre 'Buenos Aires' de la provincia.
+	select * from Localidades
+	select * from provincias
+	select m.* from multas m
+	inner join Localidades l on m.IDLocalidad = l.IDLocalidad
+	inner join Provincias p on p.IDProvincia = l.IDProvincia
+	where p.Provincia = 'Buenos Aires'
+
+
 --6 Listado con el promedio de antigüedad de los agentes que se encuentren activos.
+	select datediff(year,fechaingreso, getdate()) from agentes
+
+	select avg (datediff(year,fechaingreso, getdate())*1.0) from agentes
+	where activo = 1
+	select * from Agentes
+
 --7 Listado con el monto más elevado que se haya registrado en una multa.
+	select max(monto) from multas
+	select * from multas
+	order by monto desc
+	
+
+
 --8 Listado con el importe de pago más pequeño que se haya registrado.
+	select min(monto) from multas
 --9 Por cada agente, listar Legajo, Apellidos y Nombres y la cantidad de multas que
 --registraron.
+	select ag.legajo, ag.apellidos, ag.nombres,count (m.IdAgente) cantidad from  agentes ag
+	inner join multas m on m.IdAgente = ag.IdAgente
+	group by legajo, apellidos, nombres
+	
+	
+
+
 --10 Por cada tipo de infracción, listar la descripción y el promedio de montos de las
 --multas asociadas a dicho tipo de infracción.
+select * from  multas
+order by IdTipoInfraccion
+	select ti.descripcion, avg(m.monto*1.0)  from TipoInfracciones ti
+	inner join multas m on m.IdTipoInfraccion = ti.IdTipoInfraccion
+	group by Descripcion
+	
+
 --11 Por cada multa, indicar la fecha, la patente, el importe de la multa y la cantidad de
 --pagos realizados. Solamente mostrar la información de las multas que hayan sido
 --pagadas en su totalidad.
+		
+		select m.FechaHora, m.Patente, m.Monto, count( p.idpago) 
+		from multas m
+		inner join pagos p on p.IDMulta = m.IdMulta
+
+		select * from pagos
+
+		--update multas
+		--set pagada = 1 
+		--where IdMulta = 3
+		---SELECT * FROM MULTAS
+	--insert into multas( IdTipoInfraccion,IDLocalidad,IdAgente,Patente,FechaHora,Monto,Pagada)
+	--values(2,16,3,'abc333',getdate(),666,1)
+
+
 --12 Listar todos los datos de las multas que hayan registrado más de un pago.
+	
+	select * from multas
+	order by idmulta
+	select * from MediosPago
+	select * from pagos
+
 --13 Listar todos los datos de todos los agentes que hayan registrado multas con un
 --monto que en promedio supere los $10000
+		 
+			
+
 --14 Listar el tipo de infracción que más cantidad de multas haya registrado.
 --15 Listar por cada patente, la cantidad de infracciones distintas que se cometieron.
 --16 Listar por cada patente, el texto literal 'Multas pagadas' y el monto total de los pagos
