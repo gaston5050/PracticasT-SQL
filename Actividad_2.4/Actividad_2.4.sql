7c86a2bd9a89a7dd30ef87951d78957de5a5a467
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2023-05-30 16:47:45 -0300

    hasta el punto 5 resuelto

--- Actividad_2.4/Actividad_2.4.sql
+++ Actividad_2.4/Actividad_2.4.sql
@@ -4,6 +4,7 @@
 --# Consulta
 --1 La patente, apellidos y nombres del agente que labró la multa y monto de aquellas
 --multas que superan el monto promedio.
+	go
 	select avg(m.monto) from multas m
 	select *  from multas
 	select m.patente, ag.apellidos, ag.nombres, m.monto, (select avg(m.monto) from multas m) prome from agentes ag
@@ -12,6 +13,8 @@
 	group by patente, Apellidos, Nombres, monto
 --2 Las multas que sean más costosas que la multa más costosa por 'No respetar señal
 --de stop'.
+
+go
 			select m.* from multas m 
 			where m.monto > (select max(mul.monto )  from multas mul
 			inner join TipoInfracciones ti on ti.IdTipoInfraccion = mul.IdTipoInfraccion
@@ -36,23 +39,98 @@
 
 --3 Los apellidos y nombres de los agentes que no hayan labrado multas en los dos
 --primeros meses de 2023.
-	select ag.apellidos, ag.nombres from  agentes ag
+use AgenciaTransito
+go
+	select ag.apellidos, ag.nombres, ag.IdAgente from  agentes ag
 	where ag.IdAgente not in
-	(select idAgente from multas where month(fechahora) = 2)
-	select * from multas
+	(select idAgente from multas where month(fechahora) = 2 or month(fechahora) = 1 and year(fechahora) =2023)
+	group by  ag.nombres, ag.apellidos, ag.IdAgente
 
+	select * from multas 
+	order by fechahora desc
 
+	select * from multas 
+	order by IdAgente
+
+	go
 
 --4 Los apellidos y nombres de los agentes que no hayan labrado multas por 'Exceso de
 --velocidad'.
+
+	select ag.IdAgente,ag.apellidos, ag.nombres from agentes ag
+	where ag.IdAgente not in 
+	(select m.idagente from multas m
+	 inner join TipoInfracciones tp on tp.IdTipoInfraccion = m.IdTipoInfraccion
+	 where tp.Descripcion like 'Exceso de velocidad')
+
+
+
 --5 Los legajos, apellidos y nombre de los agentes que hayan labrado multas de todos
 --los tipos de infracciones existentes.
+	select ag.legajo, ag.apellidos, ag.nombres  from agentes ag
+	where (
+			select count(distinct ti.idtipoinfraccion) from TipoInfracciones ti
+			inner join multas m on ti.IdTipoInfraccion = m.IdTipoInfraccion
+			where ag.IdAgente = m.IdAgente) = 10
+	 
+	 
+		select count(distinct ti.idtipoinfraccion) from TipoInfracciones ti
+			inner join multas m on ti.IdTipoInfraccion = m.IdTipoInfraccion
+			where m.IdAgente = 2
+
+
+	select *  from multas
+	where IdAgente = 2
+
+
+
+		select legajo from agentes
+		inner join agentes ag on ag.IdAgente = multas.IdAgente
+		order by ag.IdAgente
+
+
 --6 Los legajos, apellidos y nombres de los agentes que hayan labrado más cantidad de
 --multas que la cantidad de multas generadas por un radar (multas con IDAgente con
 --valor NULL)
+go
+	select ag.legajo,ag.IdAgente, ag.apellidos, ag.nombres from agentes ag
+	where (select count(*) from multas m
+	where m.IdAgente = ag.idagente)> (
+		select count(*) from multas 
+		where	IdAgente  is null)
+	
+	select count(*) from agentes ag
+	inner join  multas m on ag.IdAgente = m.IdAgente
+	group by ag.IdAgente
+
+go
+
+		select * from multas 
+		order by idagente asc
+		where	IdAgente  is null
+		select * from multas
+		alter 
+
 --7 Por cada agente, listar legajo, apellidos, nombres, cantidad de multas realizadas
 --durante el día y cantidad de multas realizadas durante la noche.
 --NOTA: El turno noche ocurre pasadas las 20:00 y antes de las 05:00.
+go
+	select isnull(ag.legajo, 0), ag.apellidos, ag.nombres, ag.idagente,
+	(select count(*) from multas
+	where multas.IdAgente =ag.idagente 
+	and 
+	(datepart(hour,fechahora) > 20 or datepart(Hour,FechaHora) <5)
+	
+	
+	) multasRealilzadas from agentes ag
+
+	select count(*) from multas where 
+
+
+	select m.*, datepart(hour, m.fechahora) horita from multas m
+	order by m.IdAgente desc
+
+	go
 --8 Por cada patente, el total acumulado de pagos realizados con medios de pago no
 --electrónicos y el total acumulado de pagos realizados con algún medio de pago
 --electrónicos.
